9ee0edc82b6bdf5509a00aa404641250c3b69ad9
Lukas Kornmayer <lukas.kornmayer@web.de>
2023-11-28 15:16:26 +0100

    initial commit

---
+++ app/src/main/java/com/example/qrcodegenerator/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.qrcodegenerator;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.qrcodegenerator.databinding.ActivityMainBinding;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ActivityMainBinding binding;
+    String[] id = new String[] {
+        "hsh3j5k2051", "fd23vsd2016", "zd53uhi6120"
+    };
+
+    private ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    showCamera();
+                }
+                else {
+
+                }
+            });
+
+    private ActivityResultLauncher<ScanOptions> qrCodeLauncher = registerForActivityResult(new ScanContract(), result -> {
+        if (result.getContents() == null) {
+            Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
+        } else {
+            checkRestaurantID(result.getContents());
+        }
+    });
+
+    private void checkRestaurantID(String idDatabase) {
+
+        for (String id : id) {
+            if(id.equals(idDatabase)) {
+
+                getData(id.substring(0,7));
+                String idTable = id.substring(7);
+            } else {
+                Toast.makeText(this, "Keine ID gefunden", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void getData(String idRestaurant){
+        // ID request database
+    }
+
+    private void showCamera() {
+        ScanOptions options = new ScanOptions();
+        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE);
+        options.setPrompt("Scan QR Code");
+        options.setCameraId(0);
+        options.setBeepEnabled(false);
+        options.setBarcodeImageEnabled(true);
+        options.setOrientationLocked(false);
+
+        qrCodeLauncher.launch(options);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initBinding();
+        initViews();
+    }
+
+    private void initViews() {
+        binding.fab.setOnClickListener(view -> {
+            checkPermissionAndShowActivity(this);
+        });
+    }
+
+    private void checkPermissionAndShowActivity(Context context) {
+        if (ContextCompat.checkSelfPermission(
+                context,
+                android.Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED){
+            showCamera();
+        } else if (shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA)) {
+            Toast.makeText(context, "Camera permission required", Toast.LENGTH_SHORT).show();
+        } else {
+            requestPermissionLauncher.launch(android.Manifest.permission.CAMERA);
+        }
+    }
+
+    private void initBinding() {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
